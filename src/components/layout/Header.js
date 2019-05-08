8db2cbcbd3aadb452fcde0ef726d96ed7730b0e6
Michał Kuwik <Michal7074@poczta.fm>
2019-05-09 00:53:11 +0200

    add Link component from Route to Header.js and connect links: Home | About

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return(
       
     <header style={headerStyles}>
       <h1>Todo Aplication</h1>
+      <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
     </header>
     
   )
@@ -12,9 +14,15 @@ function Header() {
 
 const headerStyles = {
   background: '#333',
-  color:'#fff',
+  color:'#ddd',
   textAlign: 'center',
-  padding: '10px'
+  padding: '10px',
+  
+}
+
+const linkStyle = {
+  color:'#fff',
+  textDecoration: 'none',
 }
 
 export default Header;

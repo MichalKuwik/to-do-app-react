1837d70074a778016dec146daca590c52ffd4239
Michał Kuwik <Michal7074@poczta.fm>
2019-05-09 01:05:45 +0200

    add img folder and add text content and mini photo to About.js component

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import '../../css/App.css';
 
 function About() {
   return(
-    <>
+    <div style={aboutStyle} className="about">
     <h2>About me</h2>
     <p>This is my ToDoList Application</p>
-    </>
+    <p>My name is Michał Kuwik. I'm 28 years old.</p>
+    <div class="photo"></div>
+    </div>
   )
+}
+
+const aboutStyle = {
+  textAlign:'center',
 }
 
 export default About;
